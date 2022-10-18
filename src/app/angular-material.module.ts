986f2de394463784c2fb4d192b7f426d902fff16
gio <giosyrianos@gmail.com>
2022-10-18 16:52:54 +0300

    feat: outsource ang-material modules

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+
+@NgModule({
+	exports: [
+		CommonModule,
+		MatInputModule,
+		MatSelectModule,
+		MatButtonModule,
+		MatIconModule,
+		MatChipsModule,
+	]
+})
+
+export class AngularMaterialModule { }
